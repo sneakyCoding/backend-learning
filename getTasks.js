9bd485c998311293b9652b4c8063fec7863294b8
Koder Kalash <kalashmashup@gmail.com>
2026-01-26 20:50:09 +0530

    fix: implemented get tasks

--- getTasks.js
+++ getTasks.js
@@ -2,12 +2,28 @@
 
 
 export const getTasks = asyncHandler(async (req, res) => {
-    // const { projectId } = req.params
+    const { projectId } = req.params
+    const { id: userId, role } = req.params
 
-    // const project = Project.findById(projectId)
-    // if(!project) res.status(404).json({
-    //     message: "No project found"
-    // })
+    const project = await Project.findById(projectId)
+    if (!project) res.status(404).json({
+        message: "No project found"
+    })
+
+    const isOwner = project.ownerId.toString() === userId;
+    const isAdmin = req.user.role === "admin";
+
+    if (!isAdmin && !isOwner) res.status(403).json({
+        message: "Unauthorized: Access denied"
+    })
+
+    const task = await Task.find({
+        projectId,
+        completed: false
+    })
 
 
-})``
+    res.status(200).json({
+        data: task
+    })
+})
